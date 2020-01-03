0ffb8a34238e74dd0e806b8bc6092993838949dc
mubaxhir <mubashirbabar10@gmail.com>
2020-01-03 17:39:48 +0500

    piaic

---
+++ gatsby-node.js
@@ -0,0 +1,20 @@
+const path = require(`path`);
+const slash = require(`slash`);
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+  // we use the provided ContentfulHome query to fetch the data from Contentful
+  return graphql(
+    `
+      {
+        ContentfulHome {
+          edges {
+            node {
+              id
+              slug
+            }
+          }
+        }
+      }
+    `
+  )
+};
