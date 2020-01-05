7219e7ddcecf2052666afc3635761d7abea046c5
mubaxhir <mubashirbabar10@gmail.com>
2020-01-05 16:53:06 +0500

    version1.7.5

--- src/pages/artificial_intelligence.js
+++ src/pages/artificial_intelligence.js
@@ -3,40 +3,44 @@ import Layout from "../components/layout"
 import AI from "../components/AI.module.scss"
 
 
-
-export default () => <Layout>
+const Piaic = ({ data }) => {
+  
+    const {courseName,courseIntro,heading2,heading3,text,quarters,progStructure} = data.allContentfulCourse.edges[3].node;
+    
+    return (
+    <Layout>
     
     
     <div className = {AI.heading}>
-    <h1>Artificial Intelligence</h1> 
-    <p>A ONE YEAR AI PROGRAM DESIGNED FOR ABSOLUTE BEGINNERS.</p>
-    <h2>GETTING PAKISTAN READY FOR THE NEW ERA OF COMPUTING ENABLED BY THE RISE OF AI.</h2>
+<h1>{courseName}</h1> 
+<h2>{courseIntro.courseIntro}</h2>
+    {/* <h2>GETTING PAKISTAN READY FOR THE NEW ERA OF COMPUTING ENABLED BY THE RISE OF AI.</h2> */}
     </div>
 
 
     <div className = {AI.page}>
-    <h1>Program Structure</h1>
-    <p>A four-quarter AI program in Data Science, Machine Learning, </p>
-    <h2>and Deep Learning.</h2>
+<h1>{heading2}</h1>
+<p>{text}</p>
+    {/* <h2>and Deep Learning.</h2> */}
     </div>
 
 
     <div className= {AI.flex}>
     <div className= {AI.box1}>
     <h1>Quarter 1</h1>
-    <p>AI Foundations</p>
+    <p>{quarters._1}</p>
     </div>
     <div className= {AI.box2}>
     <h1>Quarter 2</h1>
-    <p>Introduction to Data Science & Deep Learning</p>
+    <p>{quarters._2}</p>
     </div>
     <div className= {AI.box3}>
     <h1>Quarter 3</h1>
-    <p>Deploying AI Solutions</p>
+    <p>{quarters._3}</p>
     </div>
     <div className= {AI.box4}>
     <h1>Quarter 4</h1>
-    <p>AI In Practice</p>
+    <p>{quarters._4}</p>
     </div>
     </div>
 
@@ -44,34 +48,35 @@ export default () => <Layout>
     <div className= {AI.line}><p></p></div>
 
 
-    <div className= {AI.h1}><p>Detailed Program Structure</p></div>
+<div className= {AI.h1}><p>{heading3}</p></div>
 
     
     <div className = {AI.parent}>
     <div className= {AI.parent1}>
     <div className= {AI.child}>
-    <h1>Version Control with Git</h1>
-    <p>You won't find a top programmer, web developer, or AI engineer who doesn't use version control. This is true because version control helps developers produce better results and makes collaboration easy. Around the world, in teams both large and small, Git is an essential part of the toolchain. This module covers Git and Github.</p>
+    <img scr={require("../components/5.png")} height="50px" width="auto"alt=""/>
+    <h1>{progStructure["_1"][0]}</h1>
+    <p>{progStructure["_1"][1]}</p>
     </div>
     <div className= {AI.child}>
-    <h1>Introduction to Linear Algebra and Statistics</h1>
-    <p>In this module, students will learn the basic mathematical and statistical concepts that are needed to practice data science and understand deep learning. Students will also implement these concepts in Python and TensorFlow.</p>
+    <h1>{progStructure["_3"][0]}</h1>
+    <p>{progStructure["_3"][1]}</p>
     </div>
     <div className= {AI.child}>
-    <h1>Data Science Essentials</h1>
-    <p>In this module, students will learn key concepts and techniques used to perform data science; including statistical analysis, data cleansing and transformation, and data visualization with Python.</p>
+    <h1>{progStructure["_5"][0]}</h1>
+    <p>{progStructure["_5"][1]}</p>
     </div>
     <div className= {AI.child5}>
-    <h1>Introduction to Linux and Bash</h1>
-    <p>Linux containers are poised to take over the world. In this module, students will begin learning with an introduction of Linux and the command line interface. For many non-technical people, the command line (also referred to as CLI, Terminal, bash, or shell) is a place of mystery. However, knowledge of only a handful of basic commands is enough to start feeling comfortable. In this module, we will cover the basic commands to get you started.</p>
+    <h1>{progStructure["_7"][0]}</h1>
+    <p>{progStructure["_7"][1]}</p>
     </div>
     <div className= {AI.child}>
-    <h1>Python Microservices Development</h1>
-    <p>In recent years, REST (REpresentational State Transfer) has emerged as the standard architectural design for web services and web APIs. In this module, students will learn how easy it is to create a RESTfulweb service using Python and Flask.</p>
+    <h1>{progStructure["_9"][0]}</h1>
+    <p>{progStructure["_9"][1]}</p>
     </div>
     <div className= {AI.child2}>
-    <h1>Artificial Intelligence (AI) in Practice</h1>
-    <p>This module is structured around a series of practical code examples, demonstrating on real world problems. PIAIC strongly believes in the value of teaching using concrete examples, anchoring theoretical ideas into actual results and tangible code patterns. These examples all rely on Keras, the Python deep learning library. Students will learn Deep Learning for computer vision, text and sequences, finance, and advanced neural network design.</p>
+    <h1>{progStructure["_11"][0]}</h1>
+    <p>{progStructure["_11"][1]}</p>
     </div>
     </div>
     
@@ -190,26 +195,74 @@ export default () => <Layout>
 
     <div className= {AI.parent2}>
     <div className= {AI.child3}>
-    <h1>Object Oriented and Functional Programming using Python</h1>
-    <p>In the second module of the program, students will learn about basic programming concepts such as lists, dictionaries, classes, functions and loops, and practice writing clean and readable code with exercises for each topic. Students will also learn how to make programs interactive and how to test code safely before adding it to a project. It is a fast-paced, thorough introduction to programming with Python 3.6 that will have students writing programs, solving problems, and making things that work in no time.</p>
+    <h1>{progStructure["_2"][0]}</h1>
+    <p>{progStructure["_2"][1]}</p>
     </div>
     <div className= {AI.child4}>
-    <h1>Advanced Python Libraries with Anaconda</h1>
-    <p>In this module, students will be introduced to Anaconda, the leading open data science platform powered by Python. The open source version of Anaconda is a high performance distribution of Python and R and includes over 100 of the most popular Python and R packages for data science, such as NumPy, Matplotlib, etc.</p>
+    <h1>{progStructure["_4"][0]}</h1>
+    <p>{progStructure["_4"][1]}</p>
     </div>
     <div className= {AI.child3}>
-    <h1>The Fundamentals of Deep Learning with Keras with TensorFlow Backend</h1>
-    <p>Before implementing deep-learning algorithms, students will first become familiar with the mathematical blocks of the neural networks theory. Students will get a chance to get their hands dirty writing some simple Keras code right away! Then they will move on to advanced deep learning concepts. This module will also cover some essential advantages of Keras to prove it’s the deep-learning library of choice.</p>
+    <h1>{progStructure["_6"][0]}</h1>
+    <p>{progStructure["_6"][1]}</p>
     </div>
     <div className= {AI.child4}>
-    <h1>Docker Deep Dive</h1>
-    <p>This module provides a soup-to-nuts learning experience for core Docker technologies, including the Docker Engine, Images, Containers, Registries, Networking, Storage, and more. All of the behind the scenes theory is explained, and all concepts are clearly demonstrated on the command line. No prior knowledge of Docker or Linux is required.</p>
+    <h1>{progStructure["_8"][0]}</h1>
+    <p>{progStructure["_8"][1]}</p>
     </div>
     <div className= {AI.child4}>
-    <h1>Build AI Microservices for ContainerDeployments</h1>
-    <p>In this module, students will learn to use containers to simplify AI solution deployments and implement continuous integration and deployment strategies.</p>
+    <h1>{progStructure["_10"][0]}</h1>
+    <p>{progStructure["_10"][1]}</p>
     </div>
     </div>
     </div>
     
 </Layout>
+
+);
+};
+
+export default Piaic;
+export const pageQuery = graphql`
+query useStaticQuery {
+    allContentfulCourse {
+        edges {
+          node {
+            courseName
+            courseIntro {
+              courseIntro
+            }
+            heading2
+            text
+            quarters {
+              _1
+              _2
+              _3
+              _4
+              _5
+              _6
+            }
+            heading3
+            progStructure {
+              _1
+              _2
+              _3
+              _4
+              _5
+              _6
+              _7
+              _8
+              _9
+              _10
+              _11
+              _12
+              _13
+              _14
+              _15
+            }
+          }
+        }
+      }
+    }    
+`;
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,31 +15,30 @@ const Piaic = ({ data }) => {
     <div className= {indexS.body} >
       
     <div className = {indexS.title}>
-    
-    <img className={indexS.presidentimg} src={president["fluid"]["src"]}/>
+    <div className = {indexS.flex}>
+    <div><img width= "386px" height= "auto" src={president["file"]["url"]}/></div>
     <div className={indexS.titleout}>
   <h1>{title}</h1>
   <h2>{title2}</h2>
   
   <p>{intro["intro"]}</p>
-  <div className= {indexS.button_padding}>
+  </div>
+    </div>
+    </div>
+    <div className= {indexS.button_padding}>
     <div>
     <a href="https://portal.piaic.org/signup" target="_blank"><p className= {indexS.button}> Apply </p></a>
     </div> 
     <div className= {indexS.line}> </div>
     <ul >
-    <li className= {indexS.font_li}>{applications}k+ 
-    </li>
-    <li className= {indexS.font_li2}>
-    Applications Recieved
-    </li>
+        <li className= {indexS.font_li}>
+  {applications}k+ 
+        </li>
+        <li className= {indexS.font_li2}>
+        Applications Recieved
+        </li>
     </ul>
     </div>
-  </div>
-  
-    </div>
-    
-    
     <div className= {indexS.heading}>
   <p>{heading2}</p>
     </div>
@@ -203,7 +202,8 @@ const Piaic = ({ data }) => {
   <h1>{heading5}</h1>
   <div className={indexS.spotlight2}>
   <div className={indexS.spotlight}>
-  <div className={indexS.image}><img src={allImages[0]["fluid"]["src"]}/></div>
+  <div className={indexS.image}>
+  </div>
   <div className={indexS.text}>
   <h2>{imageTags[0]}</h2>
 <p>{imageText1['imageText1']}</p>
@@ -211,7 +211,8 @@ const Piaic = ({ data }) => {
   </div>
 
   <div className={indexS.spotlight}>
-  <div className={indexS.image2}><img src={allImages[1]["fluid"]["src"]}/></div>
+  <div className={indexS.image2}>
+  </div>
   <div className={indexS.text}>
 <h2>{imageTags[1]}</h2>
 <p>{imageText2.imageText2}</p>
@@ -219,7 +220,8 @@ const Piaic = ({ data }) => {
   </div>
   
   <div className={indexS.spotlight}>
-  <div className={indexS.image3}><img src={allImages[2]["fluid"]["src"]}/></div>
+  <div className={indexS.image3}>
+  </div>
   <div className={indexS.text2}>
   <h2>{imageTags[2]}</h2>
   <p>{imageText3.imageText3}</p>
@@ -269,22 +271,22 @@ const Piaic = ({ data }) => {
   <h1>Locations</h1>
   <h1>PIAIC Headquarters</h1>
   <div className= {indexS.add}>
-  <div className= {indexS.x}><img src={require("../components/12.png")} width="auto" height="25px" bg="rgb(36, 49, 53)" alt="klnaznc" /></div>
+  <div className= {indexS.x}><img src={require("../components/11.svg")} width="auto" height="20px" bg="rgb(36, 49, 53)" alt="klnaznc" /></div>
   <div className= {indexS.x}><a href="https://www.google.com/maps/place/Presidential+Initiative+for+Artificial+Intelligence+and+Computing+(PIAIC)+Headquarters/@24.8613137,67.0711981,17z/data=!3m1!4b1!4m5!3m4!1s0x3eb33ff89c73993d:0x91c53ba630675f8!8m2!3d24.8612787!4d67.0733723"><p>Plot 245/2 M, Block 6 PECHS, Karachi, Sindh 75100, Pakistan</p></a></div>
   </div>
   <div className= {indexS.add}>
-  <div className= {indexS.x}><img src={require("../components/10.png")} width="auto" height="25px" bg="rgb(36, 49, 53)" alt="klnaznc" /></div>
+  <div className= {indexS.x}><img src={require("../components/10.svg")} width="auto" height="20px" bg="rgb(36, 49, 53)" alt="klnaznc" /></div>
   <div className= {indexS.x}><p>+92-308-2220203 (WhatsApp as well)</p></div>
   </div>
   <h1>PIAIC Helpline</h1>
   <div className= {indexS.add}>
-  <div className= {indexS.x}><img src={require("../components/10.png")} width="auto" height="25px" bg="rgb(36, 49, 53)" alt="klnaznc" /></div>
+  <div className= {indexS.x}><img src={require("../components/10.svg")} width="auto" height="20px" bg="rgb(36, 49, 53)" alt="klnaznc" /></div>
   <div className= {indexS.x}><p>+92-308-2220203 (WhatsApp as well)</p></div>
   </div>
   <h1>Connect with us</h1>
   <div className= {indexS.add}>
-  <div className= {indexS.x}><a href= "https://www.youtube.com/channel/UC2Makv_pLAtvrjHNgg-pBLg" target = "_blank"><img src={require("../components/8.png")} width="auto" height="25px" bg="rgb(36, 49, 53)" alt="klnaznc" /></a></div>
-  <div className= {indexS.x}><a href= "https://www.facebook.com/groups/piaic/" target = "_blank"><img src={require("../components/4.jpg")} width="auto" height="25px" bg="rgb(36, 49, 53)" alt="klnaznc" /></a></div>
+  <div className= {indexS.x}><a href= "https://www.youtube.com/channel/UC2Makv_pLAtvrjHNgg-pBLg" target = "_blank"><img src={require("../components/13.svg")} width="auto" height="20px" bg="rgb(36, 49, 53)" alt="klnaznc" /></a></div>
+  <div className= {indexS.x}><a href= "https://www.facebook.com/groups/piaic/" target = "_blank"><img src={require("../components/12.svg")} width="auto" height="20px" bg="rgb(36, 49, 53)" alt="klnaznc" /></a></div>
   </div>
   </div>
 
@@ -292,38 +294,38 @@ const Piaic = ({ data }) => {
   <div className= {indexS.address3}>
   <h1>Karachi Field Office</h1>
   <div className= {indexS.add}>
-  <div className= {indexS.x}><img src={require("../components/12.png")} width="auto" height="25px" bg="rgb(36, 49, 53)" alt="klnaznc" /></div>
+  <div className= {indexS.x}><img src={require("../components/11.svg")} width="auto" height="20px" bg="rgb(36, 49, 53)" alt="klnaznc" /></div>
   <div className= {indexS.x}><a href="https://www.google.com/maps/place/Saylani+Welfare+Head+Office./@24.8827229,67.0659646,17z/data=!3m1!4b1!4m5!3m4!1s0x3eb33eebb56aaaab:0x27ab04809ef5020a!8m2!3d24.8827229!4d67.0681533" target="_blank"><p>A-25, Bahadurabad Chowrangi, Karachi, Sindh 74800, Pakistan</p></a></div>
   </div>
   <div className= {indexS.add}>
-  <div className= {indexS.x}><img src={require("../components/10.png")} width="auto" height="25px" bg="rgb(36, 49, 53)" alt="klnaznc" /></div>
+  <div className= {indexS.x}><img src={require("../components/10.svg")} width="auto" height="20px" bg="rgb(36, 49, 53)" alt="klnaznc" /></div>
   <div className= {indexS.x}><p>+92-311-1729526</p></div>
   </div>
   <h1>Rawalpindi Field Office</h1>
   <div className= {indexS.add}>
-  <div className= {indexS.x}><img src={require("../components/12.png")} width="auto" height="25px" bg="rgb(36, 49, 53)" alt="klnaznc" /></div>
+  <div className= {indexS.x}><img src={require("../components/11.svg")} width="auto" height="20px" bg="rgb(36, 49, 53)" alt="klnaznc" /></div>
   <div className= {indexS.x}><a href="https://www.google.com/maps/place/33%C2%B037'58.2%22N+73%C2%B004'04.2%22E/@33.6328413,73.0656368,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d33.6328413!4d73.0678255?hl=en" target="_blank"><p>4th-B Rd, Satellite Town, Rawalpindi, Punjab 46300</p></a></div>
   </div>
   <div className= {indexS.add}>
-  <div className= {indexS.x}><img src={require("../components/10.png")} width="auto" height="25px" bg="rgb(36, 49, 53)" alt="klnaznc" /></div>
+  <div className= {indexS.x}><img src={require("../components/10.svg")} width="auto" height="20px" bg="rgb(36, 49, 53)" alt="klnaznc" /></div>
   <div className= {indexS.x}><p>051-4940669 / 0336-6660215</p></div>
   </div>
   <h1>Islamabad Field Office</h1>
   <div className= {indexS.add}>
-  <div className= {indexS.x}><img src={require("../components/12.png")} width="auto" height="25px" bg="rgb(36, 49, 53)" alt="klnaznc" /></div>
+  <div className= {indexS.x}><img src={require("../components/11.svg")} width="auto" height="20px" bg="rgb(36, 49, 53)" alt="klnaznc" /></div>
   <div className= {indexS.x}><a href="https://www.google.com/maps/place/33%C2%B041'47.0%22N+73%C2%B000'43.7%22E/@33.696386,73.0099453,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d33.696386!4d73.012134" target="_blank"><p>F-10 Markaz F 10/3 F-10, Islamabad, Islamabad Capital Territory</p></a></div>
   </div>
   <div className= {indexS.add}>
-  <div className= {indexS.x}><img src={require("../components/10.png")} width="auto" height="25px" bg="rgb(36, 49, 53)" alt="klnaznc" /></div>
+  <div className= {indexS.x}><img src={require("../components/10.svg")} width="auto" height="20px" bg="rgb(36, 49, 53)" alt="klnaznc" /></div>
   <div className= {indexS.x}><p>051-2223191 (WhatsApp as well)</p></div>
   </div>
   <h1>Faisalabad Field Office</h1>
   <div className= {indexS.add}>
-  <div className= {indexS.x}><img src={require("../components/12.png")} width="auto" height="25px" bg="rgb(36, 49, 53)" alt="klnaznc" /></div>
+  <div className= {indexS.x}><img src={require("../components/11.svg")} width="auto" height="20px" bg="rgb(36, 49, 53)" alt="klnaznc" /></div>
   <div className= {indexS.x}><a href="https://www.google.com/maps/place/Saylani+Mass+Training+Faisalabad/@31.411708,73.0865596,17z/data=!3m1!4b1!4m5!3m4!1s0x3922681d444b32e1:0xc3887a0e53e91f7!8m2!3d31.411708!4d73.0887483" target="_blank"><p>Saylani House, 3rd Floor, Lal Mill Chowk, Factory Area, Faisalabad, Punjab</p></a></div>
   </div>
   <div className= {indexS.add}>
-  <div className= {indexS.x}><img src={require("../components/10.png")} width="auto" height="25px" bg="rgb(36, 49, 53)" alt="klnaznc" /></div>
+  <div className= {indexS.x}><img src={require("../components/10.svg")} width="auto" height="20px" bg="rgb(36, 49, 53)" alt="klnaznc" /></div>
   <div className= {indexS.x}><p>(041) 2417281 / 0337 8659969 (WhatsApp as well)</p></div>
   </div>
   </div>
@@ -340,8 +342,8 @@ export const pageQuery = graphql`
 query MyQuery {
   contentfulHome {
     president {
-      fluid {
-        src
+      file {
+        url
       }
     }
     title
@@ -389,8 +391,8 @@ query MyQuery {
     heading6
     allImages {
       
-      fluid {
-        src
+      file {
+        url
       }
     }
   }
