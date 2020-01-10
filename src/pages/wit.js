8de239ca64a8bd0ad15cc0a0052c3ce9266c765e
mubaxhir <mubashirbabar10@gmail.com>
2020-01-10 17:53:23 +0500

    version1.9

--- src/pages/wit.js
+++ src/pages/wit.js
@@ -17,126 +17,126 @@ const Piaic = ({ data }) => {
   <p>The single most important channel for the empowerment of women is education. The Women Empowerment Division is committed to providing the best technical education in the world with the flexibility to allow just about anyone to participate and improve their lives. The primary objective of the Women Empowerment Division is to provide a platform via which women can receive education and training to jumpstart their careers in the global technology industry.</p>
   <p>The Women Inclusion in Technology project is the largest initiative on women education in Pakistan. The main of the project is to drive female participation in the technology industry in Pakistan from less than 5% to 30% over the next two years. Via this project, PIAIC is propagating awareness and training on the latest, most state-of-the-art technologies to women in Pakistan. The Women Empowerment Division will aid highly skilled women in technology to establish themselves as entrepreneurs and leaders in the technology industry not only in Pakistan but also around the world.</p> */}
 
-  <h1>WOMEN INCLUSION IN TECHNOLOGY</h1>
+<h1>{title}</h1>
   <div className={WIT.line}></div>
   <h2>PIAIC WOMEN'S GALLERY</h2>
   
   <div className={WIT.spotlight}>
-  <div className={WIT.image}><img src={require("../components/wit/01.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image2}><img src={require("../components/wit/02.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image3}><img src={require("../components/wit/03.jpg")} alt="mainIcon" /></div>
+  <div className={WIT.image} ><img src={images[43]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image2}><img src={images[39]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image3}><img src={images[37]["file"]["url"]} alt="mainIcon" /></div>
   </div>
 
   <div className={WIT.spotlight}>
-  <div className={WIT.image4}><img src={require("../components/wit/04.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image5}><img src={require("../components/wit/05.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image6}><img src={require("../components/wit/06.jpg")} alt="mainIcon" /></div>
+  <div className={WIT.image4}><img src={images[42]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image5}><img src={images[41]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image6}><img src={images[4]["file"]["url"]} alt="mainIcon" /></div>
   </div>
   
   <div className={WIT.spotlight}>
-  <div className={WIT.image7}><img src={require("../components/wit/07.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image8}><img src={require("../components/wit/08.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image9}><img src={require("../components/wit/09.jpg")} alt="mainIcon" /></div>
+  <div className={WIT.image7}><img src={images[40]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image8}><img src={images[38]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image9}><img src={images[36]["file"]["url"]} alt="mainIcon" /></div>
   </div>
 
   <div className={WIT.spotlight2}>
-  <div className={WIT.image10}><img src={require("../components/wit/10.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image11}><img src={require("../components/wit/11.jpg")} alt="mainIcon" /></div>
+  <div className={WIT.image10}><img src={images[35]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image11}><img src={images[33]["file"]["url"]} alt="mainIcon" /></div>
   </div>
 
   <div className={WIT.spotlight3}>
-  <div className={WIT.image12}><img src={require("../components/wit/12.jpg")} alt="mainIcon" /></div>
+  <div className={WIT.image12}><img src={images[34]["file"]["url"]} alt="mainIcon" /></div>
   </div>
 
   <div className={WIT.spotlight4}>
-  <div className={WIT.image13}><img src={require("../components/wit/13.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image14}><img src={require("../components/wit/14.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image15}><img src={require("../components/wit/15.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image17}><img src={require("../components/wit/17.jpg")} alt="mainIcon" /></div>
+  <div className={WIT.image13}><img src={images[32]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image14}><img src={images[30]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image15}><img src={images[29]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image17}><img src={images[31]["file"]["url"]} alt="mainIcon" /></div>
   </div>
 
   <div className={WIT.spotlight2}>
-  <div className={WIT.image18}><img src={require("../components/wit/18.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image19}><img src={require("../components/wit/19.jpg")} alt="mainIcon" /></div>
+  <div className={WIT.image18}><img src={images[28]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image19}><img src={images[5]["file"]["url"]} alt="mainIcon" /></div>
   </div>
 
   <div className={WIT.spotlight}>
-  <div className={WIT.image20}><img src={require("../components/wit/20.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image21}><img src={require("../components/wit/21.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image22}><img src={require("../components/wit/22.jpg")} alt="mainIcon" /></div>
+  <div className={WIT.image20}><img src={images[6]["file"]["url"]}alt="mainIcon" /></div>
+  <div className={WIT.image21}><img src={images[19]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image22}><img src={images[26]["file"]["url"]} alt="mainIcon" /></div>
   </div>
 
   <div className={WIT.spotlight}>
-  <div className={WIT.image23}><img src={require("../components/wit/23.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image24}><img src={require("../components/wit/24.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image25}><img src={require("../components/wit/25.jpg")} alt="mainIcon" /></div>
+  <div className={WIT.image23}><img src={images[23]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image24}><img src={images[27]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image25}><img src={images[25]["file"]["url"]} alt="mainIcon" /></div>
   </div>
 
   <div className={WIT.spotlight5}>
-  <div className={WIT.image26}><img src={require("../components/wit/26.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image27}><img src={require("../components/wit/27.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image28}><img src={require("../components/wit/28.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image29}><img src={require("../components/wit/29.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image30}><img src={require("../components/wit/30.jpg")} alt="mainIcon" /></div>
+  <div className={WIT.image26}><img src={images[17]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image27}><img src={images[22]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image28}><img src={images[24]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image29}><img src={images[21]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image30}><img src={images[20]["file"]["url"]} alt="mainIcon" /></div>
   </div>
 
   <div className={WIT.spotlight}>
-  <div className={WIT.image31}><img src={require("../components/wit/31.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image32}><img src={require("../components/wit/32.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image33}><img src={require("../components/wit/33.jpg")} alt="mainIcon" /></div>
+  <div className={WIT.image31}><img src={images[18]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image32}><img src={images[16]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image33}><img src={images[15]["file"]["url"]} alt="mainIcon" /></div>
   </div>
 
   <div className={WIT.spotlight}>
-  <div className={WIT.image34}><img src={require("../components/wit/34.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image35}><img src={require("../components/wit/35.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image36}><img src={require("../components/wit/36.jpg")} alt="mainIcon" /></div>
+  <div className={WIT.image34}><img src={images[7]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image35}><img src={images[14]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image36}><img src={images[13]["file"]["url"]} alt="mainIcon" /></div>
   </div>
 
   <div className={WIT.spotlight2}>
-  <div className={WIT.image37}><img src={require("../components/wit/37.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image38}><img src={require("../components/wit/38.jpg")} alt="mainIcon" /></div>
+  <div className={WIT.image37}><img src={images[10]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image38}><img src={images[11]["file"]["url"]} alt="mainIcon" /></div>
   </div>
 
   <div className={WIT.spotlight2}>
-  <div className={WIT.image39}><img src={require("../components/wit/39.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image40}><img src={require("../components/wit/40.jpg")} alt="mainIcon" /></div>
+  <div className={WIT.image39}><img src={images[9]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image40}><img src={images[45]["file"]["url"]} alt="mainIcon" /></div>
   </div>
 
   <div className={WIT.spotlight4}>
-  <div className={WIT.image41}><img src={require("../components/wit/41.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image42}><img src={require("../components/wit/42.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image43}><img src={require("../components/wit/43.jpg")} alt="mainIcon" /></div>
+  <div className={WIT.image41}><img src={images[46]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image42}><img src={images[50]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image43}><img src={images[51]["file"]["url"]} alt="mainIcon" /></div>
   <div className={WIT.image44}><img src={require("../components/wit/44.jpg")} alt="mainIcon" /></div>
   </div>
 
   <div className={WIT.spotlight4}>
-  <div className={WIT.image45}><img src={require("../components/wit/45.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image46}><img src={require("../components/wit/46.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image47}><img src={require("../components/wit/47.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image48}><img src={require("../components/wit/48.jpg")} alt="mainIcon" /></div>
+  <div className={WIT.image45}><img src={images[47]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image46}><img src={images[49]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image47}><img src={images[48]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image48}><img src={images[12]["file"]["url"]} alt="mainIcon" /></div>
   </div>
 
   <div className={WIT.spotlight}>
-  <div className={WIT.image49}><img src={require("../components/wit/49.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image50}><img src={require("../components/wit/50.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image51}><img src={require("../components/wit/51.jpg")} alt="mainIcon" /></div>
+  <div className={WIT.image49}><img src={images[8]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image50}><img src={images[52]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image51}><img src={images[54]["file"]["url"]} alt="mainIcon" /></div>
   </div>
   
   <div className={WIT.spotlight3}>
-  <div className={WIT.image52}><img src={require("../components/wit/52.jpg")} alt="mainIcon" /></div>
+  <div className={WIT.image52}><img src={images[55]["file"]["url"]} alt="mainIcon" /></div>
   </div>
   
   <div className={WIT.spotlight}>
-  <div className={WIT.image53}><img src={require("../components/wit/53.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image54}><img src={require("../components/wit/54.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image55}><img src={require("../components/wit/55.jpg")} alt="mainIcon" /></div>
+  <div className={WIT.image53}><img src={images[53]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image54}><img src={images[3]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image55}><img src={images[56]["file"]["url"]} alt="mainIcon" /></div>
   </div>
   
   <div className={WIT.spotlight4}>
-  <div className={WIT.image56}><img src={require("../components/wit/56.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image57}><img src={require("../components/wit/57.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image58}><img src={("../components/wit/58.jpg")} alt="mainIcon" /></div>
-  <div className={WIT.image59}><img src={require("../components/wit/59.jpg")} alt="mainIcon" /></div>
+  <div className={WIT.image56}><img src={images[0]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image57}><img src={images[1]["file"]["url"]}alt="mainIcon" /></div>
+  <div className={WIT.image58}><img src={images[57]["file"]["url"]} alt="mainIcon" /></div>
+  <div className={WIT.image59}><img src={images[2]["file"]["url"]} alt="mainIcon" /></div>
   </div>
   
   </div>
